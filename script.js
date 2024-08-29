8f34778c66b22d3722d52c977ae61533d4e1d906
Dhatchin100 <dhatchin100@gmail.com>
2024-08-29 21:20:54 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-const removeColorButton = document.getElementById("removeColorButton");
-        const colorSelect = document.getElementById("colorSelect");
-
-        removeColorButton.addEventListener("click", () => {
-            const selectedColor = colorSelect.value;
-            // Remove the selected color (you can implement this logic)
-            // Update the UI (re-render the dropdown list)
-        });
